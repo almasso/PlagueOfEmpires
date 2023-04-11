e69b4d2f7b9cdd4e2f3782296ed95a7496146816
AndreaVegaSaugar <99989921+AndreaVegaSaugar@users.noreply.github.com>
2023-04-11 09:39:45 +0200

    todas las paginas conectadas y se ajustan a la ventana

--- PlagueOfEmpires/MainGame.xaml.cs
+++ PlagueOfEmpires/MainGame.xaml.cs
@@ -26,5 +26,10 @@ namespace PlagueOfEmpires
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(PauseMenu));
+        }
     }
 }
